d797e2c386bbf116029a34ac7b21894ed24a4a28
aza <darkazazello@gmail.com>
2019-04-24 22:20:56 +0200

    Initial heroku

--- project.clj
+++ project.clj
@@ -1,7 +1,18 @@
-(defproject privatka "0.1.0-SNAPSHOT"
-  :description "Simple chat bot"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [morse "0.4.2"]])
+(defproject helloworld "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://helloworld.herokuapp.com"
+  :license {:name "FIXME: choose"
+            :url "http://example.com/FIXME"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ring/ring-devel "1.2.2"]
+                 [ring-basic-authentication "1.0.5"]
+                 [environ "0.5.0"]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [morse "0.4.2"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "privatka-bot.jar"
+  :profiles {:production {:env {:production true}}})
