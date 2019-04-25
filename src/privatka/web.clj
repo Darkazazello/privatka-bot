dc734666a5dec1ef32322c5d30a302833845caaf
aza <darkazazello@gmail.com>
2019-04-25 20:40:55 +0200

    add json

--- src/privatka/web.clj
+++ src/privatka/web.clj
@@ -38,7 +38,7 @@
               (h/message message (println "Intercepted message:" message)))
 
 (defroutes app
-           (POST "/handler" req (let  (do (println req) (map bot-api req)) ))
+           (POST "/handler" req  (do (println req) (map bot-api req) ))
            (ANY "/repl" {:as req}
              (drawbridge req))
            (GET "/" []
