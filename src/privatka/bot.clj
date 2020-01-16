b6b2e0115c86be3e9c3427d695964c74c69c74fb
aza <darkazazello@gmail.com>
2020-01-16 22:02:29 +0300

    build fix

--- src/privatka/bot.clj
+++ src/privatka/bot.clj
@@ -5,7 +5,8 @@
             [clojure.data.json :as json]
             [clojure.java.io :as io]
             [clj-http.client :as client]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.core.async :as async :refer [<! timeout chan go]]))
 
 (def token (env :token))
 (def tasks (-> "tasks.json" io/resource slurp json/read-str))
@@ -13,11 +14,19 @@
 (def counter-chat "-353786595")
 (def cbu-commands {:find-message "ш" :current-square "кк" :current-position "ку"})
 (def cbu-messages {:get-current-position "сообщите точное местоположение" :get-current-square "сообщите квадрат местоположения"})
-(def counter-commands {:blpa "блпа"})
+(def counter-commands {:blpa "бпла" :start-game "красный одуванчик" :end-game "стоп"})
 
-(def help-message "Формат сообщений в ЦБУ: \n Для сообщения найдена шифровка: Ш <шифровка>. Пример Ш 1234  
-\nДля сообщения квадрата: КК <квадрат>. Пример КК квадрат 11-55
-\nДля сообщения точного местоположения: КУ <квадрат и улитка. Пример КУ 11-55 по улитке 3")
+(def help-message "Формат сообщений в ЦБУ:
+Для сообщения найдена шифровка: Ш <шифровка> Гео <гео координаты>. Пример Ш 1234 Гео 55.11.33 37.11.22
+Для сообщения квадрата: КК <квадрат>. Пример КК квадрат 11-55
+Для сообщения точного местоположения: КУ <квадрат и улитка. Пример КУ 11-55 по улитке 3")
+
+(def contr-cbu-message "Формат сообщений в Штаб:
+Запуск БПЛА БПЛА
+Активация сети информаторов Красный одуванчик
+Отбой для сети информаторов Стоп")
+
+(def meet-message "По агентурным данным состоялась встреча ДРГ и агента, координаты - ")
 
 (def fire-warning "Срочно сообщите точное местоположение во избежания дружественного удара")
 
@@ -61,7 +70,35 @@
 
 
 (defn- send-point-to-vs [m]
-  (send-text counter-chat (str "БЛПА выявил группу ДРГ в " m)))
+  (send-text counter-chat (str "БПЛА выявил группу ДРГ в " m)))
+
+(def milisecs-to-wait 3600000)
+(def is-game-started (atom true))
+(defn what-you-want-to-do []
+  (println "working"))
+
+(defn evaluate-condition []
+  @the-condition)
+
+(defn stop-periodic-function []
+  (reset! the-condition false ))
+
+(defn ping-to-drg []
+  (go
+    (while (@is-game-started)
+      (<! (timeout milisecs-to-wait))
+      (send-text cbu-chat (::get-current-square cbu-messages)))))
+
+(defn- is-encoded-message[m]
+  (if (str/re-find #"^ш*гео*$" m)
+    :true
+    :false))
+
+(defn- send-coordinate[m]
+  (let [coor (last(str/split m #"гео"))]
+
+    )
+  )
 
 (defn process-message [chat-id message]
   (let [m (str/lower-case message)]
@@ -69,12 +106,18 @@
 
       (cond 
         (str/includes? m "help") (send-text chat-id help-message)
-        (str/includes? m (:find-message cbu-commands)) (send-new-task (retrive-data (:find-message cbu-commands) m))
+        (is-encoded-message m) (let [p1 (first (str/split m #"гео"))
+                                     p2 (last (str/split m #"гео"))]
+                                 (do
+                                  (send-text counter-chat (str/join [meet-message p2]))
+                                  (send-new-task (retrive-data (:find-message cbu-commands) p1))))
         (str/includes? m (:current-square cbu-commands)) (send-square-to-vs (retrive-data (:current-square cbu-commands) m))
         (str/includes? m (:current-position cbu-commands)) (send-point-to-vs (retrive-data (:current-position cbu-commands) m))
-        :else "zero")
-      (cond (str/includes? m "help") (send-text chat-id "Формат сообщений в Штаб: \n Запуск БЛПА БЛПА")
+        :else (send-text chat-id "Сообщение не принято, повторите передачу."))
+      (cond (str/includes? m "help") (send-text chat-id contr-cbu-message)
             (str/includes? m (:blpa counter-commands)) (send-text cbu-chat fire-warning)
-            :else "zero")))
+            (str/includes? m (:start-game counter-command)) (do (reset! is-game-started true) ping-to-drg)
+            (str/includes? m (:end-game counter-command)) (do (reset! is-game-started false))
+            :else (send-text chat-id "Сообщение не принято, повторите передачу."))))
   )
 
